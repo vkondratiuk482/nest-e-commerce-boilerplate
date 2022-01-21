6d35aa127bb3a5a8d09bd4e87e9506897abbdfd6
mokuteki225 <kondratiuk011@gmail.com>
2022-01-21 04:56:29 +0200

    refactor: removed unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
@@ -30,7 +28,5 @@ import { TokenModule } from './token/token.module';
     AuthModule,
     TokenModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
